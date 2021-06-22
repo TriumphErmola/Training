70679d139e333f092f21c1b59c1910c8912caaa2
Andrey <xramovic@yandex.ru>
2021-06-22 22:04:36 +0500

    Коллекции, сейчас Алишера 42 урок

--- src/main/java/collections/ArraysList/CollectionApp.java
+++ src/main/java/collections/ArraysList/CollectionApp.java
@@ -1,4 +1,4 @@
-package collections;
+package collections.ArraysList;
 
 import java.util.ArrayList;
 import java.util.Iterator;
--- src/main/java/collections/ArraysList/CollectionExample.java
+++ src/main/java/collections/ArraysList/CollectionExample.java
@@ -1,4 +1,4 @@
-package collections;
+package collections.ArraysList;
 
 import java.util.*;
 
